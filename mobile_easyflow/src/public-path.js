a5083e48dba68bd39d44f3885cc9f53a60ec0f9c
“Fire-Dragons” <jiangzhaolong.hn@gmail.com>
2020-12-24 10:46:25 +0800

    前端文件按模块进行拆分

--- mobile_easyflow/src/public-path.js
+++
@@ -1,5 +0,0 @@
-if (window.__POWERED_BY_QIANKUN__) {
-  const dev = process.env.NODE_ENV === 'development'
-  // eslint-disable-next-line no-undef
-  __webpack_public_path__ = dev ? window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ : process.env.VUE_APP_ASSET_PATH
-}
--- sub_easyflow/src/components/moduleForm/install.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import moduleForm from './index'
-
-moduleForm.forEach((component) => {
-  Vue.use(component)
-})
